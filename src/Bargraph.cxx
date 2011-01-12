901856baadb25d39011b6fcd719553dc9f91949c
Wesley J. Landaker <wjl@icecavern.net>
2011-01-12 11:05:26 -0700

    Disambiguate - vs >> as suggested by gcc compiler warning

--- src/Bargraph.cxx
+++ src/Bargraph.cxx
@@ -82,7 +82,7 @@ void Bargraph::tesselate(int gap) {		// Pixels between sliders
     if (child(i)->visible()) nd++;
   int dx = w()/nd;				// Slider horizontal "period"
   int width = dx*nd - gap;			// Width of slider array
-  int x0 = w()-width >> 1;			// Inset to 1st slider
+  int x0 = (w()-width) >> 1;			// Inset to 1st slider
   width = dx-gap;				// Width of individual Slider
   for (int i = 0; i < children(); i++) {	// Loop to reposition sliders
     Fl_Slider* s = (Fl_Slider*) child(i);	//   Pointer to this Slider
